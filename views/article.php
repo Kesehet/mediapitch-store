7211657710a50ea64b75efa05998652ef6fe706c
Hamood Siddiqui <70904930+Kesehet@users.noreply.github.com>
2026-08-17 17:41:20 +0530

    Add related editorial content to articles

--- views/article.php
+++ views/article.php
@@ -10,6 +10,7 @@ $breadcrumbSchema=['@context'=>'https://schema.org','@type'=>'BreadcrumbList','i
   ['@type'=>'ListItem','position'=>2,'name'=>'Blog','item'=>url('blog')],
   ['@type'=>'ListItem','position'=>3,'name'=>$post['title'],'item'=>url('blog/'.$post['slug'])],
 ]];
+$relatedItems=(new \MediaPitch\Repositories\RelatedContentRepository())->forContent((int)$post['id'],!empty($post['category_id'])?(int)$post['category_id']:null);
 ?>
 <script type="application/ld+json"><?= json_encode($articleSchema,JSON_UNESCAPED_SLASHES|JSON_UNESCAPED_UNICODE) ?></script>
 <script type="application/ld+json"><?= json_encode($breadcrumbSchema,JSON_UNESCAPED_SLASHES|JSON_UNESCAPED_UNICODE) ?></script>
@@ -24,3 +25,4 @@ $breadcrumbSchema=['@context'=>'https://schema.org','@type'=>'BreadcrumbList','i
   <?php if(!empty($post['featured_image_url'])):?><div class="container narrow"><img class="article-hero" src="<?= e($post['featured_image_url']) ?>" alt="<?= e($post['title']) ?>"></div><?php endif; ?>
   <section class="section"><div class="container narrow prose"><?= safe_html($post['body'] ?? '') ?></div></section>
 </article>
+<?php require __DIR__.'/partials/related-content.php'; ?>
